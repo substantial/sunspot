694fec6b6b49fdff2d7a179089712f9c00982535
Shaun Dern <shaun@substantial.com>
2014-01-17 17:40:18 -0500

    Throw exception on index unpersisted object

--- sunspot_rails/spec/model_spec.rb
+++ sunspot_rails/spec/model_spec.rb
@@ -20,6 +20,11 @@ describe 'ActiveRecord mixin' do
       posts = Array.new(2) { |j| PostWithDefaultScope.create! :title => (10-j).to_s }
       lambda { PostWithDefaultScope.index(:batch_size => 1) }.should_not raise_error
     end
+
+    it "should blow up if the record isn't persisted" do
+      post = Post.new
+      expect { post.index}.to raise_error
+    end
   end
 
   describe 'single table inheritence' do
@@ -41,6 +46,11 @@ describe 'ActiveRecord mixin' do
 
     it 'should immediately index and commit' do
       Post.search.results.should == [@post]
+    end
+
+    it "should blow up if the record isn't persisted" do
+      post = Post.new
+      expect { post.index!}.to raise_error
     end
   end
 
@@ -129,7 +139,7 @@ describe 'ActiveRecord mixin' do
         with :title, 'Test Post'
       end.data_accessor_for(Post).include.should == [:location]
     end
-    
+
     it 'should use the include option on the data accessor when specified' do
       @post.update_attribute(:location, Location.create)
       post = Post.search do
@@ -139,20 +149,20 @@ describe 'ActiveRecord mixin' do
 
       (Rails.version >= '3.1' ? post.association(:location).loaded? : post.loaded_location?).should be_true # Rails 3.1 removed "loaded_#{association}" method
     end
-    
+
     it 'should use the select option from search call to data accessor' do
       Post.search(:select => 'id, title, body') do
         with :title, 'Test Post'
       end.data_accessor_for(Post).select.should == 'id, title, body'
     end
-    
+
     it 'should use the select option on the data accessor when specified' do
       Post.search do
         with :title, 'Test Post'
         data_accessor_for(Post).select = 'id, title, body'
       end.results.first.attribute_names.sort.should == ['body', 'id', 'title']
     end
-    
+
     it 'should not use the select option on the data accessor when not specified' do
       Post.search do
         with :title, 'Test Post'
@@ -194,7 +204,7 @@ describe 'ActiveRecord mixin' do
       Post.search_ids.to_set.should == @posts.map { |post| post.id }.to_set
     end
   end
-  
+
   describe 'searchable?()' do
     it 'should not be true for models that have not been configured for search' do
       Location.should_not be_searchable
@@ -250,7 +260,7 @@ describe 'ActiveRecord mixin' do
       Sunspot.commit
       Post.search.results.to_set.should == @posts.to_set
     end
-    
+
   end
 
   describe 'reindex() with real data' do
@@ -272,7 +282,7 @@ describe 'ActiveRecord mixin' do
       Sunspot.commit
       Post.search.results.to_set.should == @posts.to_set
     end
-    
+
     describe "using batch sizes" do
       it 'should index with a specified batch size' do
         Post.reindex(:batch_size => 1)
@@ -283,9 +293,9 @@ describe 'ActiveRecord mixin' do
   end
 
 
-  
+
   describe "reindex()" do
-  
+
     before(:each) do
       @posts = Array.new(2) { Post.create }
     end
@@ -322,7 +332,7 @@ describe 'ActiveRecord mixin' do
           Post.search.results.should_not include(@posts.first)
         end
       end
-    
+
     end
 
     describe "when using batches" do
@@ -353,7 +363,7 @@ describe 'ActiveRecord mixin' do
       end
     end
   end
-  
+
   describe "more_like_this()" do
     before(:each) do
       @posts = [
